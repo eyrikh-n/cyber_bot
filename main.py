6832dd4b9bf99255a041c267ef0e4a0ac64a6ebd
Lapa <EugenZotov@mail.ru>
2024-05-16 17:41:06 +0700

    Все ок

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import logging
 
 from data import db_session
 from data.users import User
+import asyncio
 
-from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler, ContextTypes
-from telegram import ReplyKeyboardMarkup, Update
+from telegram.ext import (Application, MessageHandler, filters, CommandHandler, ConversationHandler, ContextTypes,
+                          CallbackQueryHandler)
+from telegram import ReplyKeyboardMarkup, Update, InlineKeyboardButton, InlineKeyboardMarkup
 
-BOT_TOKEN = '6522784356:AAHB7lKSBukJDq-Tq3SAB9mxql95Cn9Dutg'
+BOT_TOKEN = '6425799983:AAGUzo77JZPhT20_6SVFfpoD5DMcqzNE07M'
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
@@ -20,6 +22,11 @@ SCHEDULE_STATE = 4
 SEX_STATE = 5
 AGE_STATE = 6
 MENU_STATE = 7
+CHOICE = 8
+RECOMEND = 9
+
+chat_id = ''
+
 
 async def help(update, context):
     """Отправляет сообщение когда получена команда /help"""
@@ -32,6 +39,10 @@ async def stop(update, context):
 
 
 async def start(update, context):
+    global chat_id
+    chat_id_1 = update.message.chat_id
+    chat_id = chat_id_1
+    print(chat_id)
     reply_keyboard = [['Запустить']]
     markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
     await update.message.reply_text(
@@ -132,7 +143,8 @@ async def age(update, context):
     markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
     await update.message.reply_text("Меню", reply_markup=markup)
 
-    return MENU_STATE
+    # return MENU_STATE
+    return CHOICE
 
 
 def create_profile(context):
@@ -159,12 +171,49 @@ async def menu(update:Update, context:ContextTypes.DEFAULT_TYPE):
     # inline_kb_full.row(inline_invite, inline_help)
 
     return GREETING_STATE
+    # return CHOICE
 
+
+async def choice(update, context):
+    # reply_keyboard = [['Запустить новогодний адвент по цифровой гигиене']]
+    # markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
+    # await update.message.reply_text("Запустить новогодний адвент", reply_markup=markup)
+    if update.message.text == 'Мой профиль':
+        pass
+    if update.message.text == 'Рекомендации':
+        reply_keyboard = [['Запустить новогодний адвент по цифровой гигиене']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
+        await update.message.reply_text("Запустить новогодний адвент", reply_markup=markup)
+        return RECOMEND
+    if update.message.text == 'Результаты выполнения':
+        pass
+    if update.message.text == 'Пройти тест по цифровой гигиене':
+        pass
+    if update.message.text == 'Пригласить друзей':
+        pass
+    if update.message.text == 'Помощь':
+        # Может быть тут должна быть функция /help, не знаю
+        pass
+async def recomend(update, context):
+    inline_keyboard = [
+        [InlineKeyboardButton('Отметить как выполненное', callback_data='completed')],
+        [InlineKeyboardButton('Отложить выполнение', callback_data='not completed')],
+        [InlineKeyboardButton('Меню', callback_data='menu')]
+    ]
+    inline_markup = InlineKeyboardMarkup(inline_keyboard)
+    await update.message.reply_text('Демонстрация рекомендации № 1. <Имя пользователя>, текст рекомендации...', reply_markup=inline_markup)
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+    if query.data == 'menu':
+        return AGE_STATE
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("stop", stop))
     application.add_handler(CommandHandler("help", help))
+    application.add_handler(CallbackQueryHandler(button))
 
     condition = (filters.TEXT | filters.PHOTO) & ~filters.COMMAND
     greeting_handler = MessageHandler(condition, greeting)
@@ -174,6 +223,8 @@ def main():
     sex_handler = MessageHandler(condition, sex)
     age_handler = MessageHandler(condition, age)
     menu_handler = MessageHandler(condition, menu)
+    choice_handler = MessageHandler(condition, choice)
+    recomend_handler = MessageHandler(condition, recomend)
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
@@ -184,7 +235,9 @@ def main():
             SCHEDULE_STATE: [schedule_handler],
             SEX_STATE: [sex_handler],
             AGE_STATE: [age_handler],
-            MENU_STATE: [menu_handler]
+            MENU_STATE: [menu_handler],
+            CHOICE: [choice_handler],
+            RECOMEND: [recomend_handler]
         },
         fallbacks=[CommandHandler('stop', stop)]
     )
@@ -194,4 +247,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
